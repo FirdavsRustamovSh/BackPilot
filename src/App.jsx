c098717f0e7d61762d6af6db4f4a47624931386f
FirdavsRustamovSh <firdavsrustamovsh@gmail.com>
2025-05-28 15:29:34 +0500

    Logs page with data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './pages/login'
-import Logs from './pages/logs'
+import Login from './pages/Login'
+import Logs from './pages/Logs'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <h1>Vite + React</h1> */}
-      <Login/>
-      <Logs/>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/logs" element={<Logs />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
